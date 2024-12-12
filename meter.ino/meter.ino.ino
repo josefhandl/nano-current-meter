5f76fcb930054e33a43da2f548458f7883ac376e
Josef Handl <handljosef@gmail.com>
2024-12-12 09:36:49 +0100

    port for atmega328p, 4 digits, continuous measurement

--- meter.ino/meter.ino.ino
+++ meter.ino/meter.ino.ino
@@ -1,82 +1,111 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+// http://www.technoblogy.com/show?2S67
 
-// ATtiny1614
+// ATmega328p
 //----------------
-// 14-pin SOIC, 8-bit, 16kB Flash, 2kB SRAM, 256B EEPROM, 20MHz
+// 28-pin SOIC, 8-bit, 32kB Flash, 2kB SRAM, 1kB EEPROM, 20MHz (8MHz used)
 //
 //                 +----+
-//           VDD  1|O   |14  GND
-//   SS AIN4 PA4  2|    |13  PA3 AIN3 SCK EXTCLK
-// VREF AIN5 PA5  3|    |12  PA2 AIN2 MISO EVOUTA
-//      AIN6 PA6  4|    |11  PA1 AIN1 MOSI
-//      AIN7 PA7  5|    |10  PA0 AIN0 RESET UPDI
-// RxD TOSC1 PB3  6|    | 9  PB0 AIN11 SCL XDIR
-// TxD TOSC2 PB2  7|    | 8  PB1 AIN10 SDA XCK
+//           PC6  1|O   |28  PC5 ADC5
+//           PD0  2|    |27  PC4 ADC4
+//           PD1  3|    |26  PC3 ADC3
+//           PD2  4|    |25  PC2 ADC2
+//           PD3  5|    |24  PC1 ADC1
+//           PD4  6|    |23  PC0 ADC0
+//           VCC  7|    |22  GND
+//           GND  8|    |21  AREF
+//           PB6  9|    |20  VCC
+//           PB7 10|    |19  PB5
+//           PD5 11|    |18  PB4
+//           PD6 12|    |17  PB3
+//           PD7 13|    |16  PB2
+//           PB0 14|    |15  PB1
 //                 +----+
 
-
-// PA6
-// |PA5
-// ||PA4
-// |||PA3
-// ||||PA2
-// |||||PA1
-// ||||||PB3 < !!!
-// |||||||
-// ABCDEFG
-
+// PB7
+// |PB6
+// ||PB5
+// |||PB4
+// ||||PB3
+// |||||PB2
+// ||||||PB1
+// |||||||PB0
+// ||||||||
+// |ABCDEFG
+// DP (decimal point)
 
 // Seven-segment definitions
 const int charArrayLen = 17;
 char charArray[] = {
-//  ABCDEFG  Segments
-  0b1111110, // 0
-  0b0110000, // 1
-  0b1101101, // 2
-  0b1111001, // 3
-  0b0110011, // 4
-  0b1011011, // 5
-  0b1011111, // 6
-  0b1110000, // 7
-  0b1111111, // 8
-  0b1111011, // 9
-  0b0000000, // 10  Space
-  0b0000001, // 11  '-'
-  0b0001110, // 12  'L'
-  0b0011101, // 13  'o'
-  0b0110111, // 14  'H'
-  0b0000100, // 15  'i'
-  0b0011100  // 16  'u'
+//  PABCDEFG  Segments
+  0b01111110, // 0
+  0b00110000, // 1
+  0b01101101, // 2
+  0b01111001, // 3
+  0b00110011, // 4
+  0b01011011, // 5
+  0b01011111, // 6
+  0b01110000, // 7
+  0b01111111, // 8
+  0b01111011, // 9
+  0b00000000, // 10  Space
+  0b00000001, // 11  '-'
+  0b00001110, // 12  'L'
+  0b00011101, // 13  'o'
+  0b00110111, // 14  'H'
+  0b00000100, // 15  'i'
+  0b00011100, // 16  'u'
+  0b00010101, // 17  'n'
+  0b10000000  // 18  '.'
 };
 
-const int Space = 10;
-const int Dash = 11;
-const int Lo = 12;
-const int Hi = 14;
-const int uA = 16;
-const int ndigits = 3;
-volatile int Buffer[] = {Dash, Dash, Dash};
-char dp = 2;  // Decimal point position 2 (off) or 0 to 1
+const int symbol_Space = 10;
+const int symbol_Dash = 11;
+const int symbol_Lo = 12;
+const int symbol_Hi = 14;
+const int symbol_uA = 16;
+const int symbol_nA = 17;
+const int symbol_point = 18;
+const int symbol_measurement = 18;
+const int ndigits = 4;
+volatile int Buffer[] = {symbol_Dash, symbol_Dash, symbol_Dash, symbol_Dash};
+char dp = 4;  // Decimal point position 4 (off) or 0 to 1
 int digit = 0;
+bool measurement; // Last decimal point on 4th segment. Indicate an ongoing measurement
+int measurementPin = PIN_PC0;
+
+// Magic number for 1uF (foil) capacitor
+//  magicNumber = (5 x log(2) x 10^(-6)) / 2
+//                 |            \______/
+//                 Voltage      Capacitance in Farads
+int magicNumber = 1732868;
 
 // Display multiplexer **********************************************
 void DisplayNextDigit() {
-    PORTA.OUTCLR = 0x7E; // Clear all segments
-    PORTB.OUTCLR = 0b1111;
-    digit = (digit + 1) % ndigits;
+    PORTB = ~0x00;       // Reset all 8 segments (low active)
+    PORTD &= 0b00001111; // Reset left 4 segment multiplexers (high active)
+
     char segs = charArray[Buffer[digit]];
-    if (dp == digit && dp != 2) {
-        PORTB.DIRSET = (1 << 2); // Set PB2 as an output
+    // Add decimal point
+    if (dp == digit) {
+        segs |= charArray[symbol_point];
     }
-    PORTA.OUTSET = (~segs & 0x7E); // Set segments (active low)
-    PORTB.OUTSET = (1 << digit) | ((~segs & 0b1) << 3);  // Activate current digit
+
+    // Add measurement symbol
+    if (measurement && 3 == digit) {
+        segs |= charArray[symbol_measurement];
+    }
+
+    PORTB &= ~segs;                // Set all 8 segments (low active)
+    PORTD |= (1 << (7-digit));     // Set left 4 segment multiplexers (high active)
+    digit = (digit + 1) % ndigits; // Increment currently processing segment
 }
 
-// Display a three-digit decimal number
+// Display a 4-digit decimal number
 void Display(int i) {
-    for (int d = 2; d >= 0; d--) {
+    for (int d = ndigits-1-1; d >= 0; --d) {
         Buffer[d] = i % 10;
         i = i / 10;
     }
@@ -85,93 +114,74 @@ void Display(int i) {
 
 // Set up Timer/Counter to multiplex the display
 void SetupDisplay() {
-    TCA0.SINGLE.PER = 4999;  // Set period for 200Hz interrupt
-    TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV8_gc | TCA_SINGLE_ENABLE_bm; // Enable timer, prescaler=8
-    TCA0.SINGLE.INTCTRL = TCA_SINGLE_OVF_bm; // Enable overflow interrupt
+    TCCR1A = 0<<WGM10;
+    TCCR1B = 1<<WGM12 | 2<<CS10;  // Divide by 8
+    OCR1A = 4999;                 // Compare match at 200Hz
+    TIMSK1 = 1<<OCIE1A;           // Enable compare match interrupt
 }
 
 // Timer interrupt - multiplexes display
-ISR(TCA0_OVF_vect) {
+ISR(TIMER1_COMPA_vect) {
     DisplayNextDigit();
-    TCA0.SINGLE.INTFLAGS = TCA_SINGLE_OVF_bm; // Clear interrupt flag
 }
 
-
-void systemReset() {
-    // Disable interrupts
-    cli();
-    
-    // Enable the Watchdog Timer system reset
-    CCP = CCP_IOREG_gc;       // Unlock protected registers
-    WDT.CTRLA = WDT_PERIOD_8CLK_gc; // Set WDT timeout to the shortest period (8 clock cycles)
-
-    // Wait for the WDT to trigger a reset
-    while (1) {
-        // Do nothing and let the watchdog timer reset the microcontroller
-    }
+void ClearBuffer() {
+    Buffer[0] = symbol_Space;
+    Buffer[1] = symbol_Space;
+    Buffer[2] = symbol_Space;
+    Buffer[3] = symbol_Space;
+    dp = 4;
 }
 
-
-// Setup **********************************************
 void setup() {
     // Configure pins for seven-segment display
-    PORTA.DIRSET = 0x7E; // Configure PA1-PA6 as outputs
-    PORTB.DIRSET = 0b1111; // Configure PB0-PB2 + PB3 (7-seg G) as digit select outputs
-
-    // Init button for reset
-    pinMode(PIN_PA0, INPUT);
+    DDRB = 0xFF; // Configure PB0-PA7 as outputs (8 segments (include decimal point))
+    DDRD |= (1 << PD4) | (1 << PD5) | (1 << PD6) | (1 << PD7); // PD4 - PD 7 as output (4 digits)
 
     SetupDisplay();
     sei(); // Enable global interrupts
 }
 
 void loop() {
-  //DisplayNextDigit();
-  //delay(1);
+    // Charging
+    //------------------------------
+    pinMode(measurementPin, OUTPUT);
+    digitalWrite(measurementPin, HIGH);
 
-    PORTA.DIRSET = PIN7_bm;  // Configure PA7 as output
-    PORTA.OUTSET = PIN7_bm;  // Set PA7 high
+    delay(700);
 
-    Buffer[0] = Dash;
-    Buffer[1] = Dash;
-    Buffer[2] = Dash;
+    // Discharging and measuring
+    //------------------------------
+    measurement = true;
+    pinMode(measurementPin, INPUT);
 
-    delay(500);
-    PORTA.DIRCLR = PIN7_bm;  // Configure PA7 as input
     unsigned long Start = millis(), Time, nA;
-    unsigned int Initial = analogRead(PIN_PA7);
+    unsigned int Initial = analogRead(measurementPin);
     unsigned int Target = (Initial * 29) / 41;
     do {
         Time = millis() - Start;
-    } while (analogRead(PIN_PA7) > Target && Time < 100000);
+    } while (analogRead(measurementPin) > Target && Time < 100000);
+    measurement = false;
 
-    // my custom correction - i don't know why - 17328680 instead of 1732868
-    nA = 1732868 / Time;
-    dp = 2;
+    ClearBuffer();
+
+    nA = magicNumber / Time;
     if (Time >= 100000) {
-        Buffer[0] = Lo;
-        Buffer[1] = Lo + 1;
-        Buffer[2] = Space;
+        Buffer[2] = symbol_Lo;
+        Buffer[3] = symbol_Lo + 1;
     } else if (nA < 1000) {
-        dp = 2;
         Display(nA);
+        Buffer[3] = symbol_nA;
     } else if (nA < 10000) {
         dp = 0;
         Display(nA / 10);
-        Buffer[2] = uA;
+        Buffer[3] = symbol_uA;
     } else if (nA < 100000) {
-        dp = 2;
+        dp = 1;
         Display(nA / 100);
-        Buffer[2] = uA;
+        Buffer[3] = symbol_uA;
     } else {
-        Buffer[0] = Hi;
-        Buffer[1] = Hi + 1;
-        Buffer[2] = Space;
+        Buffer[2] = symbol_Hi;
+        Buffer[3] = symbol_Hi + 1;
     }
-    PORTA.DIRSET = PIN7_bm;
-    PORTA.OUTSET = PIN7_bm; // Set PA7 high
-    
-  while (!digitalRead(PIN_PA0)) {
-    delay(10);
-  }
 }
